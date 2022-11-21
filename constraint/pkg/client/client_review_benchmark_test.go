01b17e0dc3287eb4abd2ba05003446d8038e5bed
Alex Pana <8968914+acpana@users.noreply.github.com>
2022-11-21 22:24:39 +0000

    wip stage benchmark
    Signed-off-by: Alex Pana <8968914+acpana@users.noreply.github.com>

--- constraint/pkg/client/client_review_benchmark_test.go
+++ constraint/pkg/client/client_review_benchmark_test.go
@@ -1,3 +1,5 @@
+// main
+
 package client_test
 
 import (
@@ -73,7 +75,7 @@ func BenchmarkClient_Review(b *testing.B) {
 	}}
 
 	for _, templates := range []int{1, 10, 100} {
-		for _, constraints := range []int{1, 5, 10, 50, 100, 500, 1000} {
+		for _, constraints := range []int{1, 5, 10, 50, 100, 500, 1000, 10000} {
 			if templates > constraints {
 				continue
 			}
@@ -117,6 +119,117 @@ func BenchmarkClient_Review(b *testing.B) {
 						}
 					})
 				})
+			}
+		}
+	}
+}
+
+func BenchmarkClient_Review_multipleObjects(b *testing.B) {
+	tests := []struct {
+		name           string
+		review         handlertest.Review
+		makeConstraint func(tid int, name string) *unstructured.Unstructured
+	}{{
+		name: "success",
+		review: handlertest.Review{
+			Object: handlertest.Object{
+				Name: "has-bar",
+				Data: "bar",
+			},
+		},
+		makeConstraint: func(tid int, name string) *unstructured.Unstructured {
+			return cts.MakeConstraint(b, clienttest.KindCheckDataNumbered(tid), name, cts.WantData("bar"))
+		},
+	}, {
+		name: "fail",
+		review: handlertest.Review{
+			Object: handlertest.Object{
+				Name: "has-foo",
+				Data: "foo",
+			},
+		},
+		makeConstraint: func(tid int, name string) *unstructured.Unstructured {
+			return cts.MakeConstraint(b, clienttest.KindCheckDataNumbered(tid), name, cts.WantData("bar"))
+		},
+	}, {
+		name: "filtered out",
+		review: handlertest.Review{
+			Object: handlertest.Object{
+				Name:      "has-foo",
+				Namespace: "qux",
+				Data:      "foo",
+			},
+		},
+		makeConstraint: func(tid int, name string) *unstructured.Unstructured {
+			return cts.MakeConstraint(b, clienttest.KindCheckDataNumbered(tid), name,
+				cts.WantData("bar"),
+				cts.MatchNamespace("zab"))
+		},
+	}, {
+		name: "autoreject",
+		review: handlertest.Review{
+			Object: handlertest.Object{
+				Namespace: "aaa",
+				Name:      "has-foo",
+				Data:      "foo",
+			},
+		},
+		makeConstraint: func(tid int, name string) *unstructured.Unstructured {
+			return cts.MakeConstraint(b, clienttest.KindCheckDataNumbered(tid), name,
+				cts.WantData("bar"), cts.MatchNamespace("aaa"))
+		},
+	}}
+
+	for _, numObjects := range []int{1, 5, 10, 100, 500, 1000} {
+		for _, templates := range []int{1, 10, 100} {
+			for _, constraints := range []int{1, 5, 10, 50, 100, 500, 1000, 10000} {
+
+				if templates > constraints {
+					continue
+				}
+
+				for _, tt := range tests {
+					c := clienttest.New(b)
+
+					ctx := context.Background()
+					for ts := 0; ts < templates; ts++ {
+						ct := clienttest.TemplateCheckDataNumbered(ts)
+
+						_, err := c.AddTemplate(ctx, ct)
+						if err != nil {
+							b.Fatal(err)
+						}
+					}
+
+					for cs := 0; cs < constraints; cs++ {
+						// Approximately evenly distribute Constraints among Templates.
+						tid := cs % templates
+						cid := cs / templates
+
+						name := fmt.Sprintf("wantbar-%d", cid)
+						constraint := tt.makeConstraint(tid, name)
+
+						_, err := c.AddConstraint(ctx, constraint)
+						if err != nil {
+							b.Fatal(err)
+						}
+					}
+
+					b.Run(fmt.Sprintf("%d NumObjectsMatched %d Constraints %d Templates %s", numObjects, constraints, templates, tt.name), func(b *testing.B) {
+						b.ResetTimer()
+						// Run Review queries in parallel.
+						b.RunParallel(func(pb *testing.PB) {
+							for pb.Next() {
+								for i := 0; i < numObjects; i++ {
+									_, err := c.Review(ctx, tt.review)
+									if err != nil {
+										b.Fatal(err)
+									}
+								}
+							}
+						})
+					})
+				}
 			}
 		}
 	}
